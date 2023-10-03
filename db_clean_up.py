bd6df1d8a73cf40e2a1eba6c3ea1eae855f052ec
Jakub Ochalski <jjayrex@gmail.com>
2023-10-03 22:32:35 +0200

    Clean up

--- db_clean_up.py
+++ db_clean_up.py
@@ -1,2 +1,9 @@
 import pandas as pd
+
 db = pd.read_excel("FCCdb_201130_v5_Zenodo.xlsx", sheet_name=2)
+
+columns_dropped = ["CAS \nvalidity", "Synonyms, \nas used by other sources",
+                   "Priority hazardous substance prioritized based on selected authoritative sources? + why",
+                   "Substance of potential concern identified based on selected non-authoritative sources? + why"]
+
+db_cleaned = db.drop(columns=columns_dropped)
